3d56c75c4b8024b904006164a73d05f7503701f0
Nam Huynh Thanh <hthanhnam@tma.com.vn>
2021-01-08 15:35:56 +0700

    add django heroku settings

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -147,3 +147,7 @@ LANGUAGES = [
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
