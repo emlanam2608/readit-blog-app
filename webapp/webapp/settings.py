1e0c97bab83b996898f06f3322d05a9953378999
Nam Huynh Thanh <hthanhnam@tma.com.vn>
2021-01-08 18:28:33 +0700

    udpate static files handle

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -147,6 +147,7 @@ LANGUAGES = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 #  Add configuration for static files storage using whitenoise
